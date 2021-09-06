bca9a553f5ac8337e28f422d04a92c80dae6df78
Nandadulal5 <84185091+Nandadulal5@users.noreply.github.com>
2021-09-06 18:46:34 +0530

    Add files via upload

---
+++ SpringBootRest7MayApplication.java
@@ -0,0 +1,13 @@
+package com.nandadulal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRest7MayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest7MayApplication.class, args);
+	}
+
+}
